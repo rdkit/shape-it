7dd407b9620a465461194583b04006b12d5a3c95
Greg Landrum <greg.landrum@gmail.com>
2021-08-06 17:44:37 +0200

    update header (#13)

--- src/printHeader.cpp
+++ src/printHeader.cpp
@@ -54,30 +54,18 @@ void printHeader() {
   std::cerr << "  -> RDKit: " << RDKit::rdkitVersion << std::endl;
 #endif
   std::cerr << std::endl;
-  std::cerr << "  Copyright 2012 by Silicos-it, a division of Imacosi BVBA"
-            << std::endl;
-  std::cerr << std::endl;
   std::cerr
-      << "  Shape-it is free software: you can redistribute it and/or modify"
-      << std::endl;
-  std::cerr << "  it under the terms of the GNU Lesser General Public License "
-               "as published"
-            << std::endl;
-  std::cerr << "  by the Free Software Foundation, either version 3 of the "
-               "License, or"
-            << std::endl;
-  std::cerr << "  (at your option) any later version." << std::endl;
-  std::cerr << std::endl;
-  std::cerr << "  Shape-it is distributed in the hope that it will be useful,"
-            << std::endl;
-  std::cerr
-      << "  but WITHOUT ANY WARRANTY; without even the implied warranty of"
-      << std::endl;
-  std::cerr << "  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-            << std::endl;
-  std::cerr << "  GNU Lesser General Public License for more details."
-            << std::endl;
-  std::cerr << std::endl;
+      << R"DOC(  Copyright 2012-2021 by Silicos-it, a division of Imacosi BVBA, Hans De Winter,
+     and the Shape-it contributors
+
+  Shape-it is free software: you can redistribute it and/or modify
+  it under the terms of the MIT license.
+
+  Shape-it is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  MIT License for more details.
+)DOC" << std::endl;
 #ifndef USE_RDKIT
   std::cerr << "  Shape-it is linked against OpenBabel version 2." << std::endl;
   std::cerr
